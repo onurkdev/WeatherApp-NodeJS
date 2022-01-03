c37842082e712c7b8007ad7df249d397a12d8967
onurkurtulmus <onurk.dev@outlook.com>
2022-01-03 14:22:20 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'OnurK.Dev'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'OnurK.Dev'
     })
 })
 
@@ -80,7 +80,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'OnurK.Dev',
         errorMessage: 'Help article not found.'
     })
 })
@@ -88,7 +88,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'OnurK.Dev',
         errorMessage: 'Page not found.'
     })
 })
